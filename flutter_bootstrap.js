f6e69099003ae75078d6a153854cec61b4601892
Bagruti Deploy <noreply@bagruti.co.il>
2026-09-10 00:58:42 +0300

    Phase 1 gamification: streaks/XP/daily goal + confetti

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -49,6 +49,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 _flutter.loader.load({
   config: { renderer: "canvaskit" },
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3875046623" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+    serviceWorkerVersion: "2400810007" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
   },
 });
