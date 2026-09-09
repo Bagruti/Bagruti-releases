2d4045d09b80cf2a1087b58b85017d469f5caef5
Bagruti Deploy <noreply@bagruti.co.il>
2026-09-09 17:53:43 +0300

    Redeploy web: Assistant bold/extrabold typography

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -49,6 +49,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 _flutter.loader.load({
   config: { renderer: "canvaskit" },
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2058753023" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+    serviceWorkerVersion: "821040510" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
   },
 });
